1dddf22091d94933884ead2d6fc60be7e31be490
std <std@admins-Mac-mini.local>
2019-12-04 10:09:06 -0800

    gatekeeper middleware

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+function gateKeep(req,res,next) {
+  if(req.headers.password==="mellon") {
+    next();
+  }
+  else {
+    res.status(401).json("You cannot continue");
+  }
+}
+
+
 server.use(express.json());
+server.use(gateKeep);
 
 server.use('/api/hubs', hubsRouter);
 
@@ -16,5 +27,6 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
 
 module.exports = server;
